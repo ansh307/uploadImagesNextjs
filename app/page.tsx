ccde5679b35e5493b0c7af1b92f0a66237555019
ansh307 <anshsoni55333@gmail.com>
2023-10-12 13:10:26 +0530

    ddsasdas

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { storage } from "./firebase";
-import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import { ref, uploadBytes, listAll, getDownloadURL, getStorage, deleteObject } from "firebase/storage";
 import { v4 } from "uuid";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
@@ -48,6 +48,34 @@ export default function Home() {
       });
   }, []);
 
+  const handleDeleteImage = async (url: string) => {
+    try {
+      const imageRef = ref(storage, url);
+      // Delete the image from Firebase Storage
+      await deleteObject(imageRef);
+      // Remove the deleted image from the imageList
+      setImageList((prev) => prev.filter((image) => image !== url));
+    } catch (error) {
+      console.error("Error deleting image:", error);
+    }
+  };
+  
+  const handleDownloadImage = (url: string) => {
+    // Create an anchor element
+    const link = document.createElement("a");
+    link.href = url;
+    link.target = "_blank"; // Open the URL in a new tab
+    link.download = url.substring(url.lastIndexOf("/") + 1); // Set the filename based on the URL
+    link.click();
+  };
+
+
+  const handleViewImage = (url: string) => {
+    // To view the image, you can open it in a new tab/window
+    window.open(url, "_blank");
+  };
+
+
   return (
     <main className="flex items-center justify-center">
       <div className="flex">
@@ -66,7 +94,6 @@ export default function Home() {
                 className="hidden"
                 onChange={handleFileChange}
               />
-              
               <button onClick={uploadImage} className="bg-white text-black hover:bg-black border border-white hover:text-white font-bold py-2 px-4 rounded shadow cursor-pointer">
                 Upload Image
               </button>
@@ -90,11 +117,28 @@ export default function Home() {
             // Render your actual images here
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
               {imageList.map((url, index) => (
-                <div
-                  key={index}
-                  className="bg-white p-2 border border-gray-300 rounded-lg shadow h-60 w-60 transition-transform transform hover:scale-105"
-                >
-                  <img src={url} alt={`Image ${index}`} className="max-w-full max-h-full" />
+                <div key={index} className="mb-6">
+                  <div className="bg-white p-2 border border-gray-300 rounded-lg shadow h-60 w-60 transition-transform transform hover:scale-105">
+                    <img src={url} alt={`Image ${index}`} className="max-w-full max-h-full text-black" />
+                  </div>
+                  <button
+                    onClick={() => handleDownloadImage(url)}
+                    className="bg-blue-500 text-white font-bold py-2 px-1 rounded-lg mt-2 w-full"
+                  >
+                    DownLoad
+                  </button>
+                  <button
+                    onClick={() => handleViewImage(url)}
+                    className="bg-green-500 text-white font-bold py-2 px-1 rounded-lg mt-2 w-full"
+                  >
+                    View
+                  </button>
+                  <button
+                    onClick={() => handleDeleteImage(url)}
+                    className="bg-red-500 text-white font-bold py-2 px-1 rounded-lg mt-2 w-full"
+                  >
+                    Delete
+                  </button>
                 </div>
               ))}
             </div>
